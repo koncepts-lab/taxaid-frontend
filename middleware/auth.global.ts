b7b1b6b049150f1a44f98cb62ba8b9924cd32b8c
alwinkj <alwin.k@konceptslab.com>
2026-09-02 10:48:46 +0530

    fix:connecoter download and implemention download links

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -3,7 +3,7 @@ export default defineNuxtRouteMiddleware((to) => {
   const adminToken = useCookie('admin_token')
   const rpToken    = useCookie('rp_token')
 
-  const publicPages   = ['/', '/home', '/revenue-partnership-login', '/verify-email', '/ad-aqnz-pro-auth-78z46', '/admin-reset-password', '/reset-password','/ticketing/ticketing-dashboard']
+  const publicPages   = ['/', '/home', '/revenue-partnership-login', '/verify-email', '/ad-aqnz-pro-auth-78z46', '/admin-reset-password', '/reset-password','/ticketing/ticketing-dashboard', '/connector']
   const adminPrefixes = ['/admin']
   const rpPrefixes    = ['/revenue-partnership/admin', '/revenue-partnership/accounts', '/revenue-partnership/partner', '/revenue-partnership/notifications', '/revenue-partnership/select-dashboard']
 
